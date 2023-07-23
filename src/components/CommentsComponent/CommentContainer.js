1069a18c38099ea5f75f36cd9186361e54412609
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-23 13:38:48 +0300

    hw 6

--- src/components/CommentsComponent/CommentContainer.js
+++ src/components/CommentsComponent/CommentContainer.js
@@ -3,14 +3,14 @@ import {Link} from "react-router-dom";
 import styles from './CommentsComponent.css';
 import { AppRoutes } from "../../Routing/AppRoutes";
 
-const CommentContainer = ({ comment, postId }) => {
-    const { id, name, email, body } = comment;
+const CommentContainer = ({ comment }) => {
+    const { id, postId, name, email, body } = comment;
 
     return (
         <div className={styles.CommentContainer}>
             <Link to={`${AppRoutes.COMMENTS}/${postId}`}>
                 <div>
-                    <h3>{postId}</h3>
+                    <h3>postId: {postId}</h3>
                     <h3>{id}</h3>
                     <h2>{name}</h2>
                     <h2>{email}</h2>
