cd51216c605d66c0e2e1dfba6e1a367791086a4c
DinaDi021 <di.serdyuck0201@gmail.com>
2023-08-03 22:32:15 +0300

    mini-project

--- src/components/MovieInfo/MovieInfoDetails/MovieInfoDetails.js
+++ src/components/MovieInfo/MovieInfoDetails/MovieInfoDetails.js
@@ -1,18 +1,19 @@
 import styles from './MovieInfo.module.css'
 import {PosterPreview} from "../../PosterPreview/PosterPreview";
 
-
 const MovieInfoDetails = ({movie}) => {
     const {title, overview, release_date, vote_average, poster_path} = movie;
     const baseURL = 'https://image.tmdb.org/t/p/';
     const imageSize = 'w500';
     const imageURL = baseURL + imageSize + poster_path;
 
+
     return (
         <div className={styles.filmCard}>
             <div className={styles.filmInfo}>
                 <h2>{title}</h2>
-                <h2>Rating: {vote_average}</h2>
+                <h2>Rating: {vote_average}
+                </h2>
                 <div>
                     <h4>Overview:</h4>
                     <p>{overview}</p>
--- src/components/PosterPreview/PosterPreview.js
+++ src/components/PosterPreview/PosterPreview.js
@@ -3,52 +3,34 @@ import { useParams } from 'react-router-dom';
 
 import { videoService } from '../../services/videoService';
 import styles from '../MovieInfo/MovieInfoDetails/MovieInfo.module.css'
-import {Pagination} from "../Pagination/Pagination";
 
 const PosterPreview = () => {
     const [videos, setVideos] = useState([]);
     const { id, type } = useParams();
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const videoPerPage = 1;
-
     useEffect(() => {
-        videoService.getById(id, type).then(({ data }) => setVideos(data.results));
-    }, [id, type]);
+        videoService.getById(id).then(({ data }) => {
+            const trailers = data.results.filter((video) => video.type === 'Trailer');
+            setVideos(trailers);
+        });
+    }, [id]);
 
-    const indexOfLastVideo = currentPage * videoPerPage;
-    const indexOfFirstVideo = indexOfLastVideo - videoPerPage;
-    const currentVideo = videos.slice(indexOfFirstVideo, indexOfLastVideo);
-
-    const handleNextPage = () => {
-        setCurrentPage((prevPage) => prevPage + 1);
-    };
-
-    const handlePrevPage = () => {
-        setCurrentPage((prevPage) => prevPage - 1);
-    };
+    const currentVideo = videos.length > 0 ? videos[0] : null;
 
     return (
         <div className={styles.containerVideo}>
-            {currentVideo.map((video) => (
-                <div key={video.id}>
-                    <iframe
-                        width="510"
-                        height="285"
-                        src={`https://www.youtube.com/embed/${video.key}`}
-                        title={video.title}
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                    ></iframe>
+            {currentVideo && (
+                <div key={currentVideo.id}>
+            <iframe
+                width="510"
+                height="285"
+                src={`https://www.youtube.com/embed/${currentVideo.key}`}
+                title={currentVideo.title}
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+            ></iframe>
                 </div>
-            ))}
-            <Pagination
-            currentPage={currentPage}
-            itemsPerPage={videoPerPage}
-            totalItems={videos.length}
-            onNextPage={handleNextPage}
-            onPrevPage={handlePrevPage}
-        />
+            )}
         </div>
     );
 };
